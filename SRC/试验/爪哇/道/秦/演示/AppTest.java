f46da948d53a79eb86d4793b576d7364c63fb875
jy00805782 <59223243+jy00805782@users.noreply.github.com>
2020-05-27 15:04:20 +0800

    Update and rename src/test/java/tao/qin/demo/AppTest.java to SRC/试验/爪哇/道/秦/演示/AppTest.java
    fetch测试

--- SRC/试验/爪哇/道/秦/演示/AppTest.java
+++ SRC/试验/爪哇/道/秦/演示/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple App.test0001
  */
 public class AppTest 
     extends TestCase
